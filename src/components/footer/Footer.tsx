8abb0851e73d2d7c02ea77418daf6d0da7461898
tornike2002 <toko.toka2000@gmail.com>
2024-02-20 23:33:13 +0400

    added footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -9,18 +9,26 @@ import styled from "styled-components";
 const Footer: React.FC = () => {
   return (
     <FooterComponents>
-      <div>
-        <FontAwesomeIcon icon={faDiscord} />
-        <FontAwesomeIcon icon={faYoutube} />
-        <FontAwesomeIcon icon={faInstagramSquare} />
-        <FontAwesomeIcon icon={faFacebook} />
-      </div>
-      <div>
-        <a href="notice">Privacy Notice</a>
-        <a href="terms">Terms Of Service</a>
-        <a href="cookie">Cookie Policy</a>
-        <a href="information">Who we are?</a>
-      </div>
+      <Socials>
+        <SocialsHover>
+          <FontAwesomeIcon icon={faDiscord} />
+        </SocialsHover>
+        <SocialsHover>
+          <FontAwesomeIcon icon={faYoutube} />
+        </SocialsHover>
+        <SocialsHover>
+          <FontAwesomeIcon icon={faInstagramSquare} />
+        </SocialsHover>
+        <SocialsHover>
+          <FontAwesomeIcon icon={faFacebook} />
+        </SocialsHover>
+      </Socials>
+      <Coockies>
+        <CoockiesATag href="notice">Privacy Notice</CoockiesATag>
+        <CoockiesATag href="terms">Terms Of Service</CoockiesATag>
+        <CoockiesATag href="cookie">Cookie Policy</CoockiesATag>
+        <CoockiesATag href="information">Who we are?</CoockiesATag>
+      </Coockies>
     </FooterComponents>
   );
 };
@@ -36,4 +44,37 @@ const FooterComponents = styled.footer`
   flex-direction: column;
   align-items: center;
   justify-content: space-evenly;
+`;
+
+const Socials = styled.div`
+  background-color: #000000;
+  border-radius: 10px;
+  color: rgb(156, 112, 0);
+  font-size: xx-large;
+  padding: 10px;
+  display: flex;
+  gap: 25px;
+  align-items: center;
+`;
+
+const SocialsHover = styled.div`
+  &:hover {
+    color: gold;
+  }
+`;
+
+const Coockies = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  gap: 40px;
+`;
+
+const CoockiesATag = styled.a`
+  color: white;
+  text-decoration: none;
+  padding: 15px;
+  &:hover {
+    color: gold;
+  }
 `;
