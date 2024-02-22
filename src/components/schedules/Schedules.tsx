d387f1733c55bf05de6e74c0859eb8f2b82638a7
tornike2002 <toko.toka2000@gmail.com>
2024-02-22 16:35:46 +0400

    added first teams styles

--- src/components/schedules/Schedules.tsx
+++ src/components/schedules/Schedules.tsx
@@ -1,13 +1,19 @@
+import styled from "styled-components";
 import SchedulesCard from "./SchedulesCard";
 
 const Schedules: React.FC = () => {
   return (
     <div>
-      <div>
+      <SchedulesContainer>
         <SchedulesCard />
-      </div>
+      </SchedulesContainer>
     </div>
   );
 };
 
 export default Schedules;
+
+const SchedulesContainer = styled.div`
+    min-height: 100vh;
+    background-color: #0F1519;
+`
--- src/components/schedules/SchedulesCard.tsx
+++ src/components/schedules/SchedulesCard.tsx
@@ -1,19 +1,41 @@
-import FirstTeam from "/assets/schedules/firstTeam.png"
+import styled from "styled-components";
+import FirstTeam from "/assets/schedules/firstTeam.png";
 const SchedulesCard: React.FC = () => {
   return (
-    <div>
+    <SchedulesCardsContainer>
       <div>
-        <div>time</div>
-        <div>
-          <div>
-            <h1>name</h1>
-            <p>win/lose</p>
-          </div>
-          <img src={FirstTeam} alt="fTeam" />
-        </div>
+        <ScheduleTime>20:00</ScheduleTime>
       </div>
-    </div>
+      <ScheduleTeamCredsContainer>
+        <ScheduleFirstTeamLogo>
+          <h1>G2 Esports</h1>
+          <p>4W-5L</p>
+        </ScheduleFirstTeamLogo>
+        <img src={FirstTeam} alt="fTeam" />
+      </ScheduleTeamCredsContainer>
+    </SchedulesCardsContainer>
   );
 };
 
 export default SchedulesCard;
+
+const SchedulesCardsContainer = styled.div`
+  color: #fff;
+  padding-top: 120px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+const ScheduleTime = styled.h1`
+  color: #fff;
+`;
+const ScheduleTeamCredsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+const ScheduleFirstTeamLogo = styled.div`
+  color: #8fa3b0;
+  font-size: smaller;
+`;
