4b57604e1ddec16ed5f45ead345e3a69beef2a4f
ioane-tech <ioturman4@gmail.com>
2024-02-22 22:16:39 +0400

    register page

---
+++ src/components/Registration/LoginBg.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useRef } from "react";
+
+import zedRegisterBg from '../../../public/assets/zedRegisterBg.mp4'
+
+import BgVideo from "../../styled-components/bgVideo";
+
+const LoginBg = () => {
+
+
+  return (
+
+    <div>
+      <BgVideo src={zedRegisterBg} autoPlay muted loop />
+    </div>
+  );
+};
+
+export default LoginBg;
