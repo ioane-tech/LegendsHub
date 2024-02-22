d71e488b8ea52d715a8111ea142c780cbabf8ec1
tornike2002 <toko.toka2000@gmail.com>
2024-02-22 15:17:30 +0400

    fixed some unused tags

--- src/components/Router/AllRoutes.tsx
+++ src/components/Router/AllRoutes.tsx
@@ -1,20 +1,33 @@
-import React from 'react'
+import { Routes, Route } from "react-router-dom";
 
-import { Routes, Route} from "react-router-dom"
-
-import Home from '../home/Home';
-import BgVideo from '../bg-video/BgVideo';
-import LoginPage from '../Registration/LoginPage';
+import Home from "../home/Home";
+import BgVideo from "../bg-video/BgVideo";
+import LoginPage from "../Registration/LoginPage";
 
 function AllRoutes() {
   return (
     <div>
-        <Routes>
-            <Route path='/' element={<><Home/><BgVideo /></>}/>
-            <Route path='/registration' element={<><LoginPage/></>}/>
-        </Routes>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Home />
+              <BgVideo />
+            </>
+          }
+        />
+        <Route
+          path="/registration"
+          element={
+            <>
+              <LoginPage />
+            </>
+          }
+        />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default AllRoutes
+export default AllRoutes;
