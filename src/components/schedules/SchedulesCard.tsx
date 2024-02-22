b94acb523defbf645bea8c9fbcf15f0f063272f7
tornike2002 <toko.toka2000@gmail.com>
2024-02-22 16:24:05 +0400

    Aded basic structure for team

--- src/components/schedules/SchedulesCard.tsx
+++ src/components/schedules/SchedulesCard.tsx
@@ -1,5 +1,19 @@
+import FirstTeam from "/assets/schedules/firstTeam.png"
 const SchedulesCard: React.FC = () => {
-  return <div>SchedulesCard</div>;
+  return (
+    <div>
+      <div>
+        <div>time</div>
+        <div>
+          <div>
+            <h1>name</h1>
+            <p>win/lose</p>
+          </div>
+          <img src={FirstTeam} alt="fTeam" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SchedulesCard;
