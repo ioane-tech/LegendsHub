68a8b3571a5b4abebc9bb3cff3dd6554a48c4b1f
Cotne Basiashvili <basiashvilic@gmail.com>
2024-02-22 13:04:05 +0400

    added top clan card styles

--- src/components/top-clan-card/TopCardClan.tsx
+++ src/components/top-clan-card/TopCardClan.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import CLanLogo from "/assets/theme-21-border.png";
+import PlayerIconTop from "/assets/icon-position-top.png";
 
 const TopCardClan = () => {
   return (
@@ -10,6 +12,31 @@ const TopCardClan = () => {
         </video>
       </VideoContainer>
       <h2>Crew Name</h2>
+      <div>
+        <img src={CLanLogo} alt="" width={200} />
+      </div>
+      <ul>
+        <li>
+          <img src={PlayerIconTop} width={30} alt="" />
+          Player Name
+        </li>
+        <li>
+          <img src={PlayerIconTop} width={30} alt="" />
+          Player Name
+        </li>
+        <li>
+          <img src={PlayerIconTop} width={30} alt="" />
+          Player Name
+        </li>
+        <li>
+          <img src={PlayerIconTop} width={30} alt="" />
+          Player Name
+        </li>
+        <li>
+          <img src={PlayerIconTop} width={30} alt="" />
+          Player Name
+        </li>
+      </ul>
     </Container>
   );
 };
@@ -18,10 +45,25 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  color: white;
+  height: 700px;
+  position: relative;
+  ul {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    gap: 5px;
+    li {
+      display: flex;
+      align-items: center;
+      gap: 10px;
+    }
+  }
 `;
 
 const VideoContainer = styled.div`
   position: absolute;
+  top: -150px;
 `;
 
 export default TopCardClan;
