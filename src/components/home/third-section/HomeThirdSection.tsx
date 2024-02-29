9c36906fec6f760ab9e8830190fe1510ce79344b
tornike2002 <toko.toka2000@gmail.com>
2024-02-29 19:20:54 +0400

    added container styles

---
+++ src/components/home/third-section/HomeThirdSection.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+const HomeThirdSection = () => {
+  return <ThirdContainer></ThirdContainer>;
+};
+
+export default HomeThirdSection;
+const ThirdContainer = styled.div`
+  min-height: 100vh;
+  background: url("/backgrounds/footers-bg.jpg");
+  width: 100%;
+  padding-left: 10%;
+  padding-right: 10%;
+`;
