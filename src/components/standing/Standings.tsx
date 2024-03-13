26e272586dd6ab60e453fc9be75fd705362cb193
tornike2002 <toko.toka2000@gmail.com>
2024-03-13 19:15:59 +0400

    removed old standings page

--- src/components/standing/Standings.tsx
+++ src/components/standing/Standings.tsx
@@ -1,118 +1,15 @@
-import Footer from "../footer/Footer";
 import Header from "../header/Header";
-import StandingsHeader from "./StandingsHeader";
-import FirstTeam from "/assets/schedules/firstTeam.png";
-import styled from "styled-components";
 const Standings = () => {
   return (
     <>
       <Header />
-      <StandingsMainContainer>
-        <StandingsHeader />
-        <h3>Standings</h3>
-        <div>
-          <StandingListNumberic>
-            <StandingList>
-              <img src={FirstTeam} alt="fteam" />
-              <div>
-                <h1>G2 Esports</h1>
-                <h2>7w-2L</h2>
-              </div>
-            </StandingList>
-            <StandingList>
-              <img src={FirstTeam} alt="fteam" />
-              <div>
-                <h1>G2 Esports</h1>
-                <h2>7w-2L</h2>
-              </div>
-            </StandingList>
-            <StandingList>
-              <img src={FirstTeam} alt="fteam" />
-              <div>
-                <h1>G2 Esports</h1>
-                <h2>7w-2L</h2>
-              </div>
-            </StandingList>
-            <StandingList>
-              <img src={FirstTeam} alt="fteam" />
-              <div>
-                <h1>G2 Esports</h1>
-                <h2>7w-2L</h2>
-              </div>
-            </StandingList>
-            <StandingList>
-              <img src={FirstTeam} alt="fteam" />
-              <div>
-                <h1>G2 Esports</h1>
-                <h2>7w-2L</h2>
-              </div>
-            </StandingList>
-            <StandingList>
-              <img src={FirstTeam} alt="fteam" />
-              <div>
-                <h1>G2 Esports</h1>
-                <h2>7w-2L</h2>
-              </div>
-            </StandingList>
-            <StandingList>
-              <img src={FirstTeam} alt="fteam" />
-              <div>
-                <h1>G2 Esports</h1>
-                <h2>7w-2L</h2>
-              </div>
-            </StandingList>
-            <StandingList>
-              <img src={FirstTeam} alt="fteam" />
-              <div>
-                <h1>G2 Esports</h1>
-                <h2>7w-2L</h2>
-              </div>
-            </StandingList>
-          </StandingListNumberic>
-        </div>
-      </StandingsMainContainer>
-      <Footer />
+      
+    
     </>
   );
 };
 
 export default Standings;
 
-const StandingsMainContainer = styled.div`
-  padding-top: 160px;
-  padding-bottom: 60px;
-  background: #0f1519;
-  h3 {
-    color: #fff;
-    font-weight: bold;
-    padding-bottom: 20px;
-  }
-`;
 
-const StandingListNumberic = styled.ol``;
-const StandingList = styled.li`
-  display: flex;
-  align-items: center;
-  gap: 10px;
-  padding-top: 10px;
-  padding-bottom: 10px;
-  padding-left: 30px;
-  color: #fff;
-  border-top: 1px solid #8fa3b0;
-  border-bottom: 1px solid #8fa3b0;
-  transition: all 500ms ease;
-  img {
-    width: 70px;
-    height: 70px;
-  }
-  div > h1 {
-    font-size: 19px;
-  }
-  div > h2 {
-    font-size: 14px;
-    color: #8fa3b0;
-  }
-  &:hover {
-    background-color: black;
-  }
-`;
+
