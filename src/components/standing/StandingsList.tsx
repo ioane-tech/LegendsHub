79c8293700d95f2be17d718cb873860a5c6bea8a
tornike2002 <toko.toka2000@gmail.com>
2024-03-13 19:34:21 +0400

    finished standings list for now

--- src/components/standing/StandingsList.tsx
+++ src/components/standing/StandingsList.tsx
@@ -19,14 +19,26 @@ export default StandingsList;
 const StandingsListCover = styled.div`
   width: 80%;
   max-width: 100%;
+  padding-top: 15px;
+  padding-bottom: 15px;
+  border-top: 0.5px solid #f19a18;
+  border-bottom: 0.5px solid #f19a18;
   .standingsList-wrapper {
     display: flex;
     align-items: center;
     gap: 40px;
+    padding-left: 40px;
+    font-family: "Cormorant Unicase", serif;
     h1 {
       font-weight: 500;
       font-size: 24px;
       line-height: 29.06px;
+    }
+    p {
+      font-weight: 400;
+      font-size: 12px;
+      line-height: 15.83px;
+      font-family: "Roboto Slab", serif;
     }
   }
 `;
